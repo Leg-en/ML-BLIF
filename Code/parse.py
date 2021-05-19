0260091dc8adcadd2547c45dd2a0ccc56edb146f
Sterthaus <44808950+Leg-en@users.noreply.github.com>
2021-05-19 16:42:16 +0200

    parse.py added

---
+++ Code/parse.py
@@ -0,0 +1,40 @@
+import xml.etree.ElementTree as ET
+import os
+import csv
+#Konvertiert die XMLs in ein reduziertes JSON
+input = r"C:\Users\Emily\Documents\GitHub\ML-BLIF\Klassifizierung"
+data_array = []
+for item in os.listdir(input):
+    data = os.path.join(input, item)
+    tree = ET.parse(data)
+    root = tree.getroot()
+    x = root.findall("object")
+    for i in x:
+        #print("---------------------------")
+        x_min = 0
+        y_min = 0
+        x_max = 0
+        y_max = 0
+        file = root.find("filename").text
+        y = i.iter()
+        for j in y:
+            name = j.find("name")
+            bbox = j.find("bndbox")
+            if not name is None:
+                type = name.text
+            if not bbox is None:
+                x_min = bbox.find("xmin").text
+                y_min = bbox.find("ymin").text
+                x_max = bbox.find("xmax").text
+                y_max = bbox.find("ymax").text
+            d = {"file":file, "x_min":x_min, "y_min":y_min, "x_max":x_max, "y_max":y_max, "type":type}
+            data_array.append(d)
+#print(data_array)
+
+
+with open("out.csv", "w", newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=["file","x_min", "y_min","x_max","y_max","type"])
+    writer.writeheader()
+    for i in data_array:
+        print(i)
+        writer.writerow(i)
