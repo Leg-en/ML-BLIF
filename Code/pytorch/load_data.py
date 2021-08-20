5cfdf2c7ae90d7e228e61cedec2efcfcc2120e1c
Marius Sterthaus <m_ster15@uni-muenster.de>
2021-08-20 13:04:17 +0200

    pytorch integration updated

--- Code/pytorch/load_data.py
+++ Code/pytorch/load_data.py
@@ -19,12 +19,13 @@ from torchvision import transforms
 
 
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None, size=1000):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None, size=None, color=None):
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
         self.size = size
+        self.color = color
 
     def __len__(self):
         return len(self.img_labels)
@@ -32,13 +33,16 @@ class CustomImageDataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
         image = Image.open(img_path)
-        image = ImageOps.grayscale(image)
+        if self.color == "grayscale":
+            image = ImageOps.grayscale(image)
+        elif self.color == "rgb":
+            pass
         ymin, ymax, xmin, xmax = (
             self.img_labels.iloc[idx, 5], self.img_labels.iloc[idx, 7], self.img_labels.iloc[idx, 4],
             self.img_labels.iloc[idx, 6])
         image = image.crop((xmin, ymin, xmax, ymax))
         image = image.resize((self.size, self.size))
-        #Todo: Image to tensor
+        # Todo: Image to tensor
         image = transforms.ToTensor()(image).unsqueeze(0)
         label = self.img_labels.iloc[idx, 3]
         if label == "Himmel":
@@ -56,8 +60,8 @@ class CustomImageDataset(Dataset):
 
 
 def buildLoaders(annotations_file=r"C:\Users\Emily\Documents\GitHub\ML-BLIF\Code\preprocess\out.csv",
-                 img_dir=r"C:\Users\Emily\Documents\Bachelor_Drohnen_Bilder\PNG",size=28):
-    training_data = CustomImageDataset(annotations_file=annotations_file, img_dir=img_dir, size=size)
+                 img_dir=r"C:\Users\Emily\Documents\Bachelor_Drohnen_Bilder\PNG", size=28, color="rgb"):
+    training_data = CustomImageDataset(annotations_file=annotations_file, img_dir=img_dir, size=size, color=color)
     train_data, test_data = torch.utils.data.random_split(training_data, [350, 98])
     train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
     test_loader = DataLoader(test_data, batch_size=64, shuffle=True)
@@ -70,7 +74,7 @@ def test(train_loader):
     print(f"Labels batch shape: {train_labels.size()}")
     img = train_features[0].squeeze()
     label = train_labels[0]
-    #plt.imshow(img.permute(1,2,0))
+    # plt.imshow(img.permute(1,2,0))
     plt.imshow(img)
     plt.show()
     print(f"Label: {label}")
--- Code/pytorch/model_build.py
+++ Code/pytorch/model_build.py
@@ -34,27 +34,35 @@ INPUT_DIM = 256 * 256 * 3
 OUTPUT_DIM = 3
 
 
-
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+            nn.Linear(28 * 28, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
             nn.Linear(512, 10),
         )
+        self.layers = nn.Sequential(
+            nn.Linear(3000000, 192),
+            nn.ReLU(),
+            nn.Linear(192, 10)
+        )
 
     def forward(self, x):
         x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        # logits = self.linear_relu_stack(x)
+        logits = self.layers(x)
         return logits
 
 
 def main():
-    train_dataloader, test_dataloader = load_data.buildLoaders()
+    train_dataloader, test_dataloader = load_data.buildLoaders(
+        annotations_file=r"C:\Users\Emily\Documents\GitHub\ML-BLIF\Code\preprocess\out.csv",
+        img_dir=r"C:\Users\Emily\Documents\Bachelor_Drohnen_Bilder\PNG", size=1000,
+        color="rgb")  # Wenn color = grayscale ist funktioniert es schon
     model = NeuralNetwork()
     learning_rate = 1e-3
     # Initialize the loss function
@@ -66,6 +74,7 @@ def main():
         train_loop(train_dataloader, model, loss_fn, optimizer)
         test_loop(test_dataloader, model, loss_fn)
     print("Done!")
+    torch.save(model, 'model.pth')
 
 
 def train_loop(dataloader, model, loss_fn, optimizer):
@@ -98,7 +107,8 @@ def test_loop(dataloader, model, loss_fn):
 
     test_loss /= num_batches
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
 
 if __name__ == '__main__':
     main()
